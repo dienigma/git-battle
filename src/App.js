6d9aa8f736e556022af7bdf228dcde917b048f11
dienigma <joshi.chinmay09@gmail.com>
2019-04-30 20:16:30 +0530

    Working on the winner component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Compare from "./Compare";
 import User from "./User";
+import Winner from "./Winner";
 const axios = require("axios");
 
 class App extends React.Component {
   state = {
     userOneName: "",
-    userTwoName: ""
+    userTwoName: "",
+    dataOne: "",
+    dataTwo: "",
+    scoreOne: "",
+    scoreTwo: "",
+    winner: ""
   };
 
   handleChangeOne = e => {
@@ -21,21 +27,32 @@ class App extends React.Component {
     });
   };
 
-  fetchRepos = username => {
-    var encodedURI = window.encodeURI(
-      "https://api.github.com/search/users?q=" + username
-    );
-    return axios.get(encodedURI).then(function(response) {
-      console.log(response);
-      return response.data.items;
-    });
+  fetchRepos = async username => {
+    const url = `https://api.github.com/search/users?q=${username}`;
+
+    return await axios.get(url).then(response => response.data.items[0]);
   };
 
-  handleCompare = () => {
-    const resOne = this.fetchRepos(this.state.userOneName);
-    const resTwo = this.fetchRepos(this.state.userTwoName);
-    console.log(resOne);
-    console.log(resTwo);
+  handleCompare = e => {
+    e.preventDefault();
+    const resOne = this.fetchRepos(this.state.userOneName).then(data =>
+      this.setState({ dataOne: data, scoreOne: data.score })
+    );
+    const resTwo = this.fetchRepos(this.state.userTwoName).then(data =>
+      this.setState({ dataTwo: data, scoreTwo: data.score })
+    );
+  };
+
+  check = () => {
+    if (this.state.scoreOne > this.state.scoreTwo) {
+      this.setState({
+        winner: this.state.dataOne.login
+      });
+    } else {
+      this.setState({
+        winner: this.state.dataTwo.login
+      });
+    }
   };
 
   render() {
@@ -47,6 +64,12 @@ class App extends React.Component {
           handleChangeTwo={this.handleChangeTwo}
         />
         <Compare handleCompare={this.handleCompare} />
+        <Winner
+          dataone={this.state.dataOne}
+          dataTwo={this.state.dataTwo}
+          scoreOne={this.scoreOne}
+          scoreTwo={this.scoreTwo}
+        />
       </div>
     );
   }
---
+++ src/Winner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// const Winner = props => {
+//   const { dataOne, dataTwo, scoreOne, scoreTwo } = props;
+//   const check = () => {
+//     if (scoreOne > scoreTwo) {
+//       console.log(dataOne.login);
+//       return <h1>{dataOne.login}</h1>;
+//     } else {
+//       console.log(dataTwo.login);
+//       return <h1>{dataTwo.login}</h1>;
+//     }
+//   };
+//   return <div>{check}</div>;
+// };
+
+class Winner extends React.Component {
+  render() {
+    return <p>Winner is</p>;
+  }
+}
+export default Winner;
